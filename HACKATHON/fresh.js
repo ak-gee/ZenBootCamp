431c4bb465d93a91106a8911d7c3a06e473f55da
ak-gee <75403489+ak-gee@users.noreply.github.com>
2021-01-20 11:23:48 -0700

    Update fresh.js

--- HACKATHON/fresh.js
+++ HACKATHON/fresh.js
@@ -1,5 +1,5 @@
 var search = document.getElementById("search")
-var genre = document.getElementById('genre')
+var table1 = document.getElementById('table1')
 var list;
 var select = document.getElementById('breeds');
 var display = document.getElementById('disp');
@@ -7,72 +7,100 @@ var list = document.getElementById('list');
 
 
 var API_KEY = "nK8OO1nHUcWgb55BdZx";
-var FD_ENDPOINT = "newaccount161108.freshdesk.com";
 
-var PATH = "/api/v2/tickets";
-var url =  "https://" + FD_ENDPOINT + ".freshdesk.com"+ PATH;
-// url= `https://newaccount161108.freshdesk.com/api/v2/tickets/
-//${API_KEY}:XsendImmediately:true`
+var url = 'http://newaccount1611087277669.freshdesk.com/api/v2/tickets';
+var username = API_KEY;
+var password = 'X';
+var imm = true;
 
- url = 'https://newaccount1611087277669.freshdesk.com/api/v2/tickets';
-let username = API_KEY;
-let password = 'X';
-let imm = true;
+var headers = new Headers();
 
-let headers = new Headers();
+headers.append('content-Type',"application/json");
+headers.append('Authorization', 'Basic ' + btoa(username + ":" + password));
 
-
-headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
-
-response = fetch(url, {method:'GET', headers: headers })   
-        
-       
-response.then( (data) => {
-  return data.json()})
-.then(json => console.log(json))
-.catch((error) =>{
-
-console.log(error)});
  
-
+/*Create a NEW Record */
 var body = {
   'email': 'email@yourdomain.com',
   'subject': 'Ticket subject',
   'description': 'Ticket description.',
   'status': 2,
   'priority': 1,
-  'custom_fields[department]':'IT'
+  
 }
 
 
 function parseJSON(response) {
 return response.json()
 }
-
-
+ 
 fetch('https://newaccount1611087277669.freshdesk.com/api/v2/tickets', {
   method: "POST",
-  body: JSON.stringify(body),
-  headers: {"Content-type": "application/json; charset=UTF-8"}//application/json
+  
+  headers,
+  credentials:"omit",
+  mode:"cors",
+  body: JSON.stringify(body)
 })
 .then(response => response.json()) 
 .then(json => console.log(json))
-.catch(err => console.log(err));
+.catch(err => {
+  console.log(err)
+  alert(error)
+});
+ 
  
 
+/*Update a Record */
+var body = {
+  'email': 'email@yourdomain.com',
+  'subject': 'Ticket subject',
+  'description': 'Ticket description.',
+  'status': 2,
+  'priority': 2,
+  'id':13,
+  
+}
 
-// Request.auth({
-//   user: API_KEY,
-//   pass: "X",
-//   sendImmediately: true
-// })
-// .end(function(response){
-//     console.log(response.body)
-//     console.log("Response Status : " +response.status)
-//     if(response.status != 200){
-//       console.log("X-Request-Id :" + response.headers['x-request-id']);
-//     }
-//   });
+fetch('https://newaccount1611087277669.freshdesk.com/api/v2/tickets', {
+  method: "PUT",
+  
+  headers,
+  credentials:"omit",
+  mode:"cors",
+  body: JSON.stringify(body)
+})
+.then(response => response.json()) 
+.then(json => {
+  // alert(json[1])
+  console.log(json[0].id)
+})
+.catch(err => {
+  console.log(err)
+  alert(error)
+});
+ 
+
+/*Delete a record */
+
+fetch('https://newaccount1611087277669.freshdesk.com/api/v2/tickets/13', {
+  method: "DELETE",
+  
+  headers,//: {"Content-type": "application/json; charset=UTF-8"}//application/json
+  credentials:"omit",
+  mode:"cors",
+  // body: JSON.stringify(body)
+})
+.then(response => response.json()) 
+.then(json => {
+  
+  console.log(json)
+})
+.catch(err => {
+  console.log(err)
+  alert('error')});
+
+
 
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
@@ -96,56 +124,19 @@ function New(id) {
       }
     }
   }
-search.addEventListener("click",function(){
+  search.addEventListener("click",function(){
+    response = fetch(url, {method:'GET', 
+    headers: headers,
+    credentials:"omit",
+    mode:"cors" })   
 
-    // list = fetch(`https://binaryjazz.us/wp-json/genrenator/v1/genre/`)
- 
-list.then((alldogs) => { 
-    
-   return  alldogs.json();
-  
-}).then((alldogs) => {
-    
-    let data = alldogs;
-    
- 
-   genre.value = data;
- 
 
-    
- 
-})
-.catch((error) => {
+    response.then( (data) => {
+    // console.log( data.json())
+    return data.json()})
+    .then(json => console.log(json))
+    .catch((error) =>{
+
     console.log(error)
-})
-  
-    // random = fetch(`https://binaryjazz.us/wp-json/genrenator/v1/story/20`)
-  
-    random.then((music) => {
-          return(music.json())
-       })       
-    .then((music) =>{
-  
-    let val = ""
-   
-    for (let t=0;t<20;t++){
-         val = val + `${music[t]}<br>`
-         var li = document.createElement('li')
-         var textnode = document.createTextNode(`${music[t]}`);
-   
-         li.appendChild(textnode);
-       
-        document.getElementById('list').appendChild(li)
-    }
-    
-    
-     })
-   
-  
-    .catch((error) => {
-        console.log(error)
-
-    })
-    
-
-});
+  alert(error)});
+    });
